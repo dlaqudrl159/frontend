eaab9a13e48b874c2b920690d24ce48b38886ead
dlaqudrl159 <qkfkaxkrl@gmail.com>
2024-11-24 05:00:59 +0900

    빈칸 제거

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,4 +1,4 @@
-import React, {memo, useState, useCallback } from 'react';
+import React, { memo, useState, useCallback } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import KakaoMap from '../kakaomap/KakaoMap';
 import Category from '../category/Category';
@@ -7,34 +7,28 @@ import Header from '../header/Header';
 
 const Home = memo(() => {
 
-    const [categoryRegionState, setCategoryRegionState] = useState(null);
-  
-    const [selectedMarkerData, setSelectedMarkerData] = useState(null);
-    
-    const handleMarkerData = useCallback((data) => {
-      setSelectedMarkerData(data);
-    }, []);
-  
-    const setCategoryRegion = useCallback((region) => {
-      setCategoryRegionState(region);
-    }, []);
-  
-    return (
-      <>
-        <Header />
-        <div className="Home" style={{ height: "95%" }}>
-          <KakaoMap setCategoryRegion={setCategoryRegion} handleMarkerData={handleMarkerData} />
-          {categoryRegionState && <Category categoryRegionState={categoryRegionState} />}
-          {selectedMarkerData && <AptTranscationHistory selectedMarkerData={selectedMarkerData} setSelectedMarkerData={setSelectedMarkerData} />}
-        </div>
-  
-  
-  
-  
-  
-  
-      </>
-    );
-  });
+  const [categoryRegionState, setCategoryRegionState] = useState(null);
+
+  const [selectedMarkerData, setSelectedMarkerData] = useState(null);
+
+  const handleMarkerData = useCallback((data) => {
+    setSelectedMarkerData(data);
+  }, []);
+
+  const setCategoryRegion = useCallback((region) => {
+    setCategoryRegionState(region);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <div className="Home" style={{ height: "95%" }}>
+        <KakaoMap setCategoryRegion={setCategoryRegion} handleMarkerData={handleMarkerData} />
+        {categoryRegionState && <Category categoryRegionState={categoryRegionState} />}
+        {selectedMarkerData && <AptTranscationHistory selectedMarkerData={selectedMarkerData} setSelectedMarkerData={setSelectedMarkerData} />}
+      </div>
+    </>
+  );
+});
 
 export default Home;
