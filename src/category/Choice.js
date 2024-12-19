f91e6a1a27db106c763f3e24cc6b4a5dd077a580
dlaqudrl159 <qkfkaxkrl@gmail.com>
2024-12-19 17:34:31 +0900

    Choice.js 스타일컴포넌트 작업

--- src/category/Choice.js
+++ src/category/Choice.js
@@ -1,75 +1,23 @@
 import React, { memo } from "react";
 import TabDropDown from "./tab/TabDropDown";
 import { Box } from "@mui/material";
+import { ChoiceContainer, ChoiceButton, ChoiceButtonContent } from "../styles/Category.Styles";
 
 const Choice = memo(({ searchType, handleSelectState }) => {
 
   return (
     <TabDropDown>
-      <Box sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-      }}>
-        <Box
-          sx={{
-            border: '1px solid #ccc',
-            borderRadius: '4px',
-            cursor: 'pointer',
-            backgroundColor: 'white',
-            width: '45%',
-            height: '40px',
-            textAlign: 'center',
-            fontSize: '16px',
-            ...(searchType === 'jibun' && {
-              backgroundColor: '#007bff',
-              color: 'white',
-              border: '1px solid #0056b3'
-            })
-          }}
-          onClick={() => handleSelectState()}
-        >지번 검색
-        </Box>
-        <Box
-          sx={{
-            border: '1px solid #ccc',
-            borderRadius: '4px',
-            cursor: 'pointer',
-            backgroundColor: 'white',
-            width: '45%',
-            height: '40px',
-            textAlign: 'center',
-            fontSize: '16px',
-            ...(searchType === 'road' && {
-              backgroundColor: '#007bff',
-              color: 'white',
-              border: '1px solid #0056b3'
-            })
-          }}
-          onClick={() => handleSelectState()}
-        >도로명 검색
-        </Box>
-      </Box>
+      <ChoiceContainer className="choiceContainer">
+        <ChoiceButton variant={searchType === 'jibun' ? "contained" : "text"} onClick={handleSelectState}>
+          <ChoiceButtonContent>지번 검색</ChoiceButtonContent>
+        </ChoiceButton>
+        <ChoiceButton variant={searchType === 'road' ? "contained" : "text"} onClick={handleSelectState}>
+          <ChoiceButtonContent>도로명 검색</ChoiceButtonContent>
+        </ChoiceButton>
+      </ChoiceContainer>
     </TabDropDown>
   )
 
 })
-
-const styles = {
-  searchTypeButton: {
-    padding: '8px 15px',
-    border: '1px solid #ccc',
-    borderRadius: '4px',
-    cursor: 'pointer',
-    backgroundColor: 'white',
-    width: '45%',
-    textAlign: 'center',
-  },
-  activeSearchType: {
-    backgroundColor: '#007bff',
-    color: 'white',
-    border: '1px solid #0056b3',
-  },
-}
 
 export default Choice;
