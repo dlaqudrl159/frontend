4ce891b4aaaa4e3eb097870d23fcd2a698b614de
dlaqudrl159 <qkfkaxkrl@gmail.com>
2025-01-04 02:09:54 +0900

    Home컴포넌트 state제거

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,35 +1,14 @@
-import React, { memo, useState, useCallback } from 'react';
+import React, { memo } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import KakaoMap from '../kakaomap/KakaoMap';
-import Category from '../category/Category';
-import AptTranscationHistory from '../aptTranscationHistory/AptTranscationHistory';
-import { Box } from '@mui/material';
-
+import { HomeContainer } from '../styles/Home.Styles';
 const Home = memo(() => {
-
-  const [categoryRegionState, setCategoryRegionState] = useState(null);
-
-  const [selectedMarkerData, setSelectedMarkerData] = useState(null);
-
-  const handleMarkerData = useCallback((data) => {
-    setSelectedMarkerData(data);
-  }, []);
-
-  const setCategoryRegion = useCallback((region) => {
-    setCategoryRegionState(region);
-  }, []);
 
   return (
     <>
-      <Box sx={{
-        width: '100%',
-        height: '100%',
-        position: 'relative'
-      }}>
-        <KakaoMap setCategoryRegion={setCategoryRegion} handleMarkerData={handleMarkerData} />
-        {categoryRegionState && <Category categoryRegionState={categoryRegionState} setSelectedMarkerData={setSelectedMarkerData} />}
-        {selectedMarkerData && <AptTranscationHistory selectedMarkerData={selectedMarkerData} setSelectedMarkerData={setSelectedMarkerData} />}
-      </Box>
+      <HomeContainer>
+        <KakaoMap/>
+      </HomeContainer>
     </>
   );
 });
